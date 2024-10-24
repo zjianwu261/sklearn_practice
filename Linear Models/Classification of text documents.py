ae702c452c3a7d0d83a7c90606e19e6c82903a68
zjianwu261 <zjianwu261@gmail.com>
2024-10-24 15:11:45 +0800

    Linear Models

---
+++ Linear Models/Classification of text documents.py
@@ -0,0 +1,32 @@
+from time import time
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+categories = [
+    "alt.atheism",
+    "talk.religion.misc",
+    "comp.graphics",
+    "sci.space",
+]
+
+
+def size_mb(docs):
+    return sum(len(s.encode("utf-8")) for s in docs) / 1e6
+
+
+def load_dataset(verbose=False, remove=()):
+    data_train = fetch_20newsgroups(
+        subset="train",
+        categories=categories,
+        shuffle=True,
+        random_state=42,
+        remove=remove,
+    )
+
+    data_test = fetch_20newsgroups(
+        subset="test",
+        categories=categories,
+        shuffle=True,
+        random_state=42,
+        remove=remove,
+    )
